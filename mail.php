8750d676011b0f2e52c7a17031470effca1de03f
DharshaUV <90187947+DharshaUV@users.noreply.github.com>
2021-09-17 17:15:12 +0530

    updated

--- mail.php
+++ mail.php
@@ -1,61 +1,138 @@
 <?php
-  if (isset($_POST["submit"])) {
-  $name = $_POST['name'] ;
-  $email = $_POST['email'] ;
-  $city = $_POST['city'] ;
-  $mobnum = $_POST['mobnum'] ;
-  $pincode = $_POST['pincode'] ;
-  $insta = $_POST['instra'] ;
-  $message =  $_POST['message'] ;
+       $name = $_POST['name'] ;
+       $email = $_POST['email'] ;
+       $city = $_POST['city'] ;
+       $mobnum = $_POST['mobnum'] ;
+       $pincode = $_POST['pincode'] ;
+       $instra = $_POST['instra'] ;
+       $stdata =  $_POST['message'] ;
+      
 
-         $to = "dharshini92.somu@gmail.com";
-         $from = "evolutebioscience@gmail.com";
-         $subject = "Students Enqurid"  ;
-         
-         $body = "<html>
-                  <head>
-                  <title>Students Enquiry Details</title>
-                  </head>
-                  <body>
-                  
-                  <table>
-                  <tr>
-                  <th>Name </th>
-                  <th>Email id</th>
-                  <th>Mobile Number</th>
-                  <th>City</th>
-                   <th>Pincode</th>
-                  <th>Instra / Linkedin ID</th>
-                   <th>Message</th>
-                 
-                  </tr>
-                  <tr>
-                            <td >$name</td>
-                            <td><h3>$email</h3></td>
-                             <td >$city</td>
-                            <td >$mobnum</td>
-                            
-                            <td >$pincode</td>
-                            <td ><h3>$instra</h3></td>
- 
-                            <td >$message</td>
-                  </tr>
-                  </table>
-                  </body>
-                  </html>";
-         
-         $header = "From: $from \r\n";
-     //    $header .= "Cc:afgh@somedomain.com \r\n";
-         $header .= "MIME-Version: 1.0\r\n";
-         $header .= "Content-type: text/html\r\n";
-         
-         $retval = mail ($to,$subject,$body,$header);
-         
-         if( $retval == true  {
-             $result =  " Thank You! Your request has been submitted successfully. " ;
-         }else {
-               $result = " Sorry there was an error sending your message. Please try again later " ;
-         }
-       }
- 
-      ?>  
+ $message  = "<html><body>";
+   
+   $message .= "<table width='100%' bgcolor='#e0e0e0' cellpadding='0' cellspacing='0' border='0'>";
+   
+   $message .= "<tr><td>";
+   
+   $message .= "<table align='center' width='100%' border='0' cellpadding='0' cellspacing='0' style='max-width:650px; background-color:#fff; font-family:Verdana, Geneva, sans-serif;'>";
+    
+   $message .= "<thead>
+      <tr height='80'>
+       <th colspan='4' style='background-color:#f5f5f5; border-bottom:solid 1px #bdbdbd; font-family:Verdana, Geneva, sans-serif; color:#333; font-size:34px;' >PSR Trust</th>
+      </tr>
+      </thead>";
+    
+   $message .= "<tbody>
+      <tr align='center' height='50' style='font-family:Verdana, Geneva, sans-serif;'>
+       <td style='background-color:#00a2d1; text-align:center;'>
+         Name 
+       </td>
+       
+       <td style='background-color:#00a2d1; text-align:center;'>
+         $name
+       </td>
+         </tr>
+   
+        <tr align='center' height='50' style='font-family:Verdana, Geneva, sans-serif;'>
+       <td style='background-color:#00a2d1; text-align:center;'>
+         Email
+       </td>
+       
+       <td style='background-color:#00a2d1; text-align:center;'>
+         $email
+       </td>
+       
+       
+      </tr>
+      
+      
+        <tr align='center' height='50' style='font-family:Verdana, Geneva, sans-serif;'>
+       <td style='background-color:#00a2d1; text-align:center;'>
+        City
+       </td>
+       
+       <td style='background-color:#00a2d1; text-align:center;'>
+       $city
+       </td>
+       
+       
+      </tr>
+      <tr align='center' height='50' style='font-family:Verdana, Geneva, sans-serif;'>
+      <td style='background-color:#00a2d1; text-align:center;'>
+       Mobile Number
+      </td>
+      
+      <td style='background-color:#00a2d1; text-align:center;'>
+      $mobnum
+      </td>
+      
+      
+     </tr>
+     <tr align='center' height='50' style='font-family:Verdana, Geneva, sans-serif;'>
+     <td style='background-color:#00a2d1; text-align:center;'>
+     Pincode
+     </td>
+     
+     <td style='background-color:#00a2d1; text-align:center;'>
+     $pincode
+     </td>
+     
+     
+    </tr>
+    <tr align='center' height='50' style='font-family:Verdana, Geneva, sans-serif;'>
+    <td style='background-color:#00a2d1; text-align:center;'>
+     Instra / Linkedin ID
+    </td>
+    
+    <td style='background-color:#00a2d1; text-align:center;'>
+      $instra
+    </td>
+    
+    
+   </tr>
+      
+      
+       <tr align='center' height='50' style='font-family:Verdana, Geneva, sans-serif;'>
+       <td style='background-color:#00a2d1; text-align:center;'>
+        Details
+       </td>
+       
+       <td style='background-color:#00a2d1; text-align:center;'>
+         $message
+       </td>
+       
+       
+      </tr>
+      
+      
+     
+      
+     
+      
+      </tbody>";
+    
+   $message .= "</table>";
+   
+   $message .= "</td></tr>";
+   $message .= "</table>";
+   
+   $message .= "</body></html>";
+
+
+    $to="dharshini92.somu@gmail.com";   
+    // $from="evolutebioscience@gmail.com" 
+    $subject="Evolutebioscience";
+    $headers = "Content-type:text/html;charset=UTF-8" . "\r\n";
+    mail($to,$subject,$message,$headers);      
+
+	
+	
+	echo "<script>
+alert('Request Send Successfully');
+window.location.href='index.html';
+</script>";
+
+
+
+
+?>
